04a6b2fb9199a41dab5fbd5b9b30d7e33846a6f5
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-11-20 09:48:53 +0530

    Update Borrower.java

---
+++ src/main/java/com/jbk/entities/Borrower.java
@@ -0,0 +1,76 @@
+package com.jbk.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Borrower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+
+    @ManyToMany(mappedBy = "borrowers")
+    private List<Book> books = new ArrayList<> ( );
+
+    public Borrower () {
+    }
+
+    public Borrower (int id, String name, String email, List<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.books = books;
+    }
+
+    public Borrower (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId () {
+        return id;
+    }
+
+    public void setId (int id) {
+        this.id = id;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getEmail () {
+        return email;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public List<Book> getBooks () {
+        return books;
+    }
+
+    public void setBooks (List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString () {
+        return "Borrowers [" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", books=" + books +
+                ']';
+    }
+}
+
